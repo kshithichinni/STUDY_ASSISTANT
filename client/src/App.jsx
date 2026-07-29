c7e17b2c11852393faf3efa30b5eeb979c5407ed
kshithichinni <kshithija_yatham@srmap.edu.in>
2026-07-29 06:59:36 +0530

    Improve navigation with automatic scroll to top

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,19 @@
 import { Routes, Route } from "react-router-dom";
+import ScrollToTop from "./components/ScrollToTop";
 import Home from "./pages/Home";
 import Flashcards from "./pages/Flashcards";
 import Quiz from "./pages/Quiz";
 
 function App() {
   return (
+  <>
+  <ScrollToTop />
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/flashcards" element={<Flashcards />} />
       <Route path="/quiz" element={<Quiz />} />
     </Routes>
+  </>
   );
 }
 
